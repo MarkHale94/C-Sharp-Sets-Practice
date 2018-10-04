0f8a7444e0554dfc94439b5257daabdc075cb80a
Mark Hale <mark.hale94@gmail.com>
2018-10-04 11:04:35 -0500

    did the exercise that used the unionwith and the intersectwith methods on sets

--- Program.cs
+++ Program.cs
@@ -14,13 +14,13 @@ namespace sets {
                 
                 Showroom.Add ("Mitsubishi Lancer-Evo");
                 
-                Console.WriteLine ("In the Showroom:");
+                // Console.WriteLine ("In the Showroom:");
 
-                foreach (string model in Showroom) {
+                // foreach (string model in Showroom) {
 
-                    Console.WriteLine (model);
+                //     Console.WriteLine (model);
                 
-                }
+                // }
 
             HashSet<string> UsedLot = new HashSet<string> ();
             
@@ -28,30 +28,84 @@ namespace sets {
                 
                 UsedLot.Add ("Porsche Cayman S");
 
-                Console.WriteLine ("In the UsedLot:");
+                // Console.WriteLine ("In the UsedLot:");
                 
-                foreach (string model in UsedLot) {
+                // foreach (string model in UsedLot) {
 
-                     Console.WriteLine (model);
+                //      Console.WriteLine (model);
                      
-                }
+                // }
             
             Showroom.UnionWith(UsedLot);
 
-            Console.WriteLine ("In the Showroom:");
+            // Console.WriteLine ("In the Showroom:");
+
+            // foreach (string model in Showroom) {
+
+            //     Console.WriteLine (model);
+                
+            // }
+
+            // Console.WriteLine ("In the UsedLot:");
+
+            // foreach (string model in UsedLot) {
+
+            //     Console.WriteLine (model);
+                     
+            // }
+
+            Showroom.Remove("Nissan Maxima");
+
+            // Console.WriteLine ("In the Showroom:");
+
+            // foreach (string model in Showroom) {
+
+            //     Console.WriteLine (model);
+                
+            // }
+
+            HashSet<string> Junkyard = new HashSet<string> ();
+            
+            Junkyard.Add ("Nissan GTR");
+                
+            Junkyard.Add ("Mitsubishi Lancer-Evo");
+
+            Junkyard.Add ("Toyota Camry");
+                
+            Junkyard.Add ("Ford Mustang");
+
+            Junkyard.Add ("Mazda RX8");
+                
+            Junkyard.Add ("Dodge Charger");
+
+            HashSet<string> clone = new HashSet<string>(Showroom);
+
+            clone.IntersectWith(Junkyard);
+
+            Showroom.UnionWith(Junkyard);
+
+            // Console.WriteLine ("In the Junkyard:");
+
+            // foreach (string model in Junkyard) {
+
+            //     Console.WriteLine (model);
+                
+            // }
+
+            // Console.WriteLine ("In the Cloned Showroom:");
+
+            // foreach (string model in clone) {
+
+            //     Console.WriteLine (model);
+                
+            // }
+
+            Console.WriteLine ("In the Original Showroom:");
 
             foreach (string model in Showroom) {
 
                 Console.WriteLine (model);
                 
-            }
-
-            Console.WriteLine ("In the UsedLot:");
-            
-            foreach (string model in UsedLot) {
-
-                Console.WriteLine (model);
-                     
             }
         }
     }
