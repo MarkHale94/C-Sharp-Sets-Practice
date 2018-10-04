ad1c560588087b05baafb9b45b4e6d2f41179f96
Mark Hale <mark.hale94@gmail.com>
2018-10-04 10:47:49 -0500

    practice with sets using showroom and used lot car models

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sets {
+    class Program {
+        static void Main (string[] args) {
+            HashSet<string> Showroom = new HashSet<string> ();
+
+                Showroom.Add ("Ferrari Enzo");
+
+                Showroom.Add ("Mercedes SLR-McClarin");
+                
+                Showroom.Add ("Nissan GTR");
+                
+                Showroom.Add ("Mitsubishi Lancer-Evo");
+                
+                Console.WriteLine ("In the Showroom:");
+
+                foreach (string model in Showroom) {
+
+                    Console.WriteLine (model);
+                
+                }
+
+            HashSet<string> UsedLot = new HashSet<string> ();
+            
+                UsedLot.Add ("Nissan Maxima");
+                
+                UsedLot.Add ("Porsche Cayman S");
+
+                Console.WriteLine ("In the UsedLot:");
+                
+                foreach (string model in UsedLot) {
+
+                     Console.WriteLine (model);
+                     
+                }
+            
+            Showroom.UnionWith(UsedLot);
+
+            Console.WriteLine ("In the Showroom:");
+
+            foreach (string model in Showroom) {
+
+                Console.WriteLine (model);
+                
+            }
+
+            Console.WriteLine ("In the UsedLot:");
+            
+            foreach (string model in UsedLot) {
+
+                Console.WriteLine (model);
+                     
+            }
+        }
+    }
+}
